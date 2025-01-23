78752a389fd7cab543d095de53973f98cce1cf69
jblSomoza <jhony-bryan@outlook.com>
2025-01-22 22:42:36 -0600

    Agregar componente MovieHeader y mejorar la pantalla de detalles de la película

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -2,26 +2,30 @@ import { View, Text, ActivityIndicator, ScrollView } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import { useMovie } from '@/presentation/hooks/useMovie';
+import MovieHeader from '@/presentation/components/movie/MovieHeader';
 
 const MovieScreen = () => {
-    const { id } = useLocalSearchParams();
-    const { movieQuery } = useMovie(+id);
+  const { id } = useLocalSearchParams();
+  const { movieQuery } = useMovie(+id);
 
-
-    if(movieQuery.isLoading) {
-      return (
-        <View className='flex flex-1 justify-center items-center'>
-          <Text className='mb-4'>Espere por favor</Text>
-          <ActivityIndicator color={'purple'} size={34} />
-        </View>
-      )
-    }
+  if (movieQuery.isLoading || !movieQuery.data) {
+    return (
+      <View className="flex flex-1 justify-center items-center">
+        <ActivityIndicator color={"purple"} size={34} />
+        <Text className="mb-4">Espere por favor</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView>
-      <Text>{movieQuery.data?.title ?? 'No tiene'}</Text>
+      <MovieHeader
+        original_title={movieQuery.data.original_title}
+        poster={movieQuery.data.poster}
+        title={movieQuery.data.title}
+      />
     </ScrollView>
-  )
-}
+  );
+};
 
 export default MovieScreen
---
+++ presentation/components/movie/MovieHeader.tsx
@@ -0,0 +1,60 @@
+import {
+  View,
+  Text,
+  useWindowDimensions,
+  Image,
+  Pressable,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { router } from "expo-router";
+
+interface MovieHeaderProps {
+  poster: string;
+  original_title: string;
+  title: string;
+}
+
+const MovieHeader = ({ poster, original_title, title }: MovieHeaderProps) => {
+  const { height: screenHeight } = useWindowDimensions();
+
+  return (
+    <>
+      <View style={{
+        position: 'absolute',
+        top: 40,
+        left: 20,
+        zIndex: 99,
+        elevation: 9
+      }}>
+        <Pressable
+            onPress={() => { router.dismiss() }}
+        >
+          <Ionicons name="chevron-back" size={24} color="white" />
+        </Pressable>
+      </View>
+
+      <View
+        className="shadow-xl shadow-black/20"
+        style={{
+          height: screenHeight * 0.7,
+        }}
+      >
+        <View className="flex-1 rounded-b-[25px] overflow-hidden">
+          <Image
+            source={{ uri: poster }}
+            resizeMode="cover"
+            className="flex-1"
+          />
+        </View>
+      </View>
+
+      <View className="px-5 mt-5">
+        <Text className="text-2xl font-bold mt-4">{original_title}</Text>
+        <Text className="text-lg font-semibold text-gray-500">{title}</Text>
+      </View>
+    </>
+  );
+};
+
+export default MovieHeader;
